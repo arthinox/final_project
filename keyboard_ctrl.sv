abe0997f29dc995c80551356485c139d9bafb4ef
Victor Lu <donkey0319@gmail.com>
2023-04-13 14:11:34 -0500

    add HexDriver and modify keyboard_ctrl.sv

--- keyboard_ctrl.sv
+++ keyboard_ctrl.sv
@@ -1,6 +1,14 @@
 module keyboard_ctrl {
+	input [7:0] keycode,
+	output 
 };
 
 // Code that takes keyboard input and converts into data used by waveform generator
+
+always_comb
+	begin
+		case (keycode)
+		
+	end
 
 endmodule
