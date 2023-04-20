dc1d42680e33738eeae879e9f1e642964f0622a3
Victor Lu <donkey0319@gmail.com>
2023-04-19 23:51:47 -0500

    tmp2

---
+++ software/new_thing/obj/default/usb_kb/MAX3421E.d
@@ -0,0 +1,48 @@
+obj/default/usb_kb/MAX3421E.o: usb_kb/MAX3421E.c \
+ ../new_thing_bsp/system.h ../new_thing_bsp/linker.h \
+ usb_kb/../usb_kb/project_config.h \
+ usb_kb/../usb_kb/../usb_kb/GenericMacros.h \
+ usb_kb/../usb_kb/../usb_kb/GenericTypeDefs.h \
+ usb_kb/../usb_kb/../usb_kb/HID.h usb_kb/../usb_kb/../usb_kb/MAX3421E.h \
+ usb_kb/../usb_kb/../usb_kb/transfer.h \
+ usb_kb/../usb_kb/../usb_kb/usb_ch9.h usb_kb/../usb_kb/../usb_kb/USB.h \
+ ../new_thing_bsp//drivers/inc/altera_avalon_spi.h \
+ ../new_thing_bsp//HAL/inc/alt_types.h \
+ ../new_thing_bsp//drivers/inc/altera_avalon_spi_regs.h \
+ ../new_thing_bsp//HAL/inc/io.h ../new_thing_bsp//HAL/inc/alt_types.h \
+ ../new_thing_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../new_thing_bsp//HAL/inc/sys/alt_stdio.h
+
+../new_thing_bsp/system.h:
+
+../new_thing_bsp/linker.h:
+
+usb_kb/../usb_kb/project_config.h:
+
+usb_kb/../usb_kb/../usb_kb/GenericMacros.h:
+
+usb_kb/../usb_kb/../usb_kb/GenericTypeDefs.h:
+
+usb_kb/../usb_kb/../usb_kb/HID.h:
+
+usb_kb/../usb_kb/../usb_kb/MAX3421E.h:
+
+usb_kb/../usb_kb/../usb_kb/transfer.h:
+
+usb_kb/../usb_kb/../usb_kb/usb_ch9.h:
+
+usb_kb/../usb_kb/../usb_kb/USB.h:
+
+../new_thing_bsp//drivers/inc/altera_avalon_spi.h:
+
+../new_thing_bsp//HAL/inc/alt_types.h:
+
+../new_thing_bsp//drivers/inc/altera_avalon_spi_regs.h:
+
+../new_thing_bsp//HAL/inc/io.h:
+
+../new_thing_bsp//HAL/inc/alt_types.h:
+
+../new_thing_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../new_thing_bsp//HAL/inc/sys/alt_stdio.h:
---
+++ software/new_thing/obj/default/usb_kb/transfer.d
@@ -0,0 +1,54 @@
+obj/default/usb_kb/transfer.o: usb_kb/transfer.c \
+ usb_kb/../usb_kb/project_config.h \
+ usb_kb/../usb_kb/../usb_kb/GenericMacros.h \
+ usb_kb/../usb_kb/../usb_kb/GenericTypeDefs.h \
+ usb_kb/../usb_kb/../usb_kb/HID.h usb_kb/../usb_kb/../usb_kb/MAX3421E.h \
+ usb_kb/../usb_kb/../usb_kb/transfer.h \
+ usb_kb/../usb_kb/../usb_kb/usb_ch9.h usb_kb/../usb_kb/../usb_kb/USB.h \
+ ../new_thing_bsp//drivers/inc/altera_avalon_timer.h \
+ ../new_thing_bsp//HAL/inc/alt_types.h \
+ ../new_thing_bsp//HAL/inc/sys/alt_dev.h ../new_thing_bsp/system.h \
+ ../new_thing_bsp/linker.h ../new_thing_bsp//HAL/inc/sys/alt_llist.h \
+ ../new_thing_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../new_thing_bsp//HAL/inc/sys/alt_warning.h \
+ ../new_thing_bsp//HAL/inc/sys/alt_alarm.h \
+ ../new_thing_bsp//HAL/inc/sys/alt_llist.h \
+ ../new_thing_bsp//HAL/inc/priv/alt_alarm.h
+
+usb_kb/../usb_kb/project_config.h:
+
+usb_kb/../usb_kb/../usb_kb/GenericMacros.h:
+
+usb_kb/../usb_kb/../usb_kb/GenericTypeDefs.h:
+
+usb_kb/../usb_kb/../usb_kb/HID.h:
+
+usb_kb/../usb_kb/../usb_kb/MAX3421E.h:
+
+usb_kb/../usb_kb/../usb_kb/transfer.h:
+
+usb_kb/../usb_kb/../usb_kb/usb_ch9.h:
+
+usb_kb/../usb_kb/../usb_kb/USB.h:
+
+../new_thing_bsp//drivers/inc/altera_avalon_timer.h:
+
+../new_thing_bsp//HAL/inc/alt_types.h:
+
+../new_thing_bsp//HAL/inc/sys/alt_dev.h:
+
+../new_thing_bsp/system.h:
+
+../new_thing_bsp/linker.h:
+
+../new_thing_bsp//HAL/inc/sys/alt_llist.h:
+
+../new_thing_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../new_thing_bsp//HAL/inc/sys/alt_warning.h:
+
+../new_thing_bsp//HAL/inc/sys/alt_alarm.h:
+
+../new_thing_bsp//HAL/inc/sys/alt_llist.h:
+
+../new_thing_bsp//HAL/inc/priv/alt_alarm.h:
